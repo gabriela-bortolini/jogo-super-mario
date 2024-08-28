a7519c0f4934fb8cae3faeb21a54b4b12381bfa2
gabriela-bortolini <gabriela.ramos.bortolini@escola.pr.gov.br>
2024-08-28 11:33:30 -0300

    .

--- man.js
+++ man.js
@@ -10,8 +10,26 @@ const jump = ( ) = > {
 ,500);
 }
 
-const loop = setInterval(() => {});
+const loop = setInterval(() => {
+   const pipePosition = pipe.offsetLeft;
+   const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
 
+   if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+      pipe.style.animation = "nome";
+      pipe.style.left = `${pipePosition}px`;
+
+      mario.style.animation = "nome";
+      mario.style.left = `${pipePosition}px`;
+
+      mario.src = "./img/game-ouver.png";
+      mario.style.width = "75px";
+      mario.style.marginLeft = "50px";
+
+      clearInterval(loop);
+   }
+},10);
+
+document.addEventListener("keydown", jump);
 
 
 
