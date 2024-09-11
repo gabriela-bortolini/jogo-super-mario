00101cc5bf170018c185f872de7b2e09b08a11a3
gabriela-bortolini <gabriela.ramos.bortolini@escola.pr.gov.br>
2024-09-11 11:14:02 -0300

    Update man.js

--- man.js
+++ man.js
@@ -2,17 +2,31 @@ const mario = document.querySelector
 const pipe = document.querySelector(".pipe");
 
 const jump = ( ) = > {
+
+    audioStart = new Audio("./sound/audio_themer.mp3");
+    const gameOverSound = new Audio("./sound/audio_gameouver.mp3");
+
+   let gameStarted = false
     mario.classList.add('jump');
 
  setTimeout(() = > {
     mario.classList.remove("jump");
  } 
+            
 ,500);
 }
 
 const loop = setInterval(() => {
    const pipePosition = pipe.offsetLeft;
    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+
+
+    const jump = () => {
+        if(gameStarted) {
+
+
+            setTimeout(() => {
+                mario.classList.add("jump");
 
    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
       pipe.style.animation = "nome";
